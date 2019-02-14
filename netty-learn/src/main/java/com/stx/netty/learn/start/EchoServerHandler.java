a2a2007a77ec3cfdbd4f4cee14085879de453282
松楚 <tongxiang.stx@alibaba-inc.com>
2019-02-14 16:26:44 +0800

    netty

---
+++ netty-learn/src/main/java/com/stx/netty/learn/start/EchoServerHandler.java
@@ -0,0 +1,31 @@
+package com.stx.netty.learn.start;
+
+/**
+ * @author tongxiang.stx
+ * @date 2019/02/14
+ */
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+public class EchoServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        System.out.println("Server received: " + msg);
+        ctx.write(msg);
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        ctx.writeAndFlush(Unpooled.EMPTY_BUFFER).addListener(ChannelFutureListener.CLOSE);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+}
